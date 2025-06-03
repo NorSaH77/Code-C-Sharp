6917226bec3d1203a6621afcadaffc93f11498a2
NorSaH <167691649+NorSaH77@users.noreply.github.com>
2025-06-03 12:00:26 +0700

    Add files via upload

---
+++ shutdownpc/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace shutdownpc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.nud1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nud3 = new System.Windows.Forms.NumericUpDown();
+            this.nud2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nud1
+            // 
+            this.nud1.AllowDrop = true;
+            this.nud1.Location = new System.Drawing.Point(128, 58);
+            this.nud1.Name = "nud1";
+            this.nud1.Size = new System.Drawing.Size(52, 20);
+            this.nud1.TabIndex = 0;
+            this.nud1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Giờ";
+            // 
+            // nud3
+            // 
+            this.nud3.AllowDrop = true;
+            this.nud3.Location = new System.Drawing.Point(326, 58);
+            this.nud3.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.nud3.Name = "nud3";
+            this.nud3.Size = new System.Drawing.Size(52, 20);
+            this.nud3.TabIndex = 2;
+            this.nud3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // nud2
+            // 
+            this.nud2.AllowDrop = true;
+            this.nud2.Location = new System.Drawing.Point(227, 58);
+            this.nud2.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.nud2.Name = "nud2";
+            this.nud2.Size = new System.Drawing.Size(52, 20);
+            this.nud2.TabIndex = 1;
+            this.nud2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AllowDrop = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(285, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Phút";
+            // 
+            // label3
+            // 
+            this.label3.AllowDrop = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(384, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Giây";
+            // 
+            // btn1
+            // 
+            this.btn1.AllowDrop = true;
+            this.btn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn1.Location = new System.Drawing.Point(155, 100);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(90, 29);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "Hẹn giờ";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.AllowDrop = true;
+            this.btn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn2.Location = new System.Drawing.Point(251, 100);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(90, 29);
+            this.btn2.TabIndex = 7;
+            this.btn2.Text = "Huỷ hẹn giờ";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(514, 211);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nud2);
+            this.Controls.Add(this.nud3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nud1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hẹn giờ tắt máy";
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nud1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nud3;
+        private System.Windows.Forms.NumericUpDown nud2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
---
+++ shutdownpc/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using System;
+using System.Diagnostics;
+
+namespace shutdownpc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            string cmd = $"shutdown -s -t {nud1.Value * 3600 + nud2.Value * 60 + nud3.Value}";
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = $"/C {cmd}";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForExit();
+            MessageBox.Show($"Máy tính sẽ tắt trong {nud1.Value} giờ {nud2.Value} phút {nud3.Value} giây!");
+        }
+
+        private void btn2_Click(object sender, System.EventArgs e)
+        {
+            string cmd = $"shutdown -a";
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = $"/C {cmd}";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForExit();
+            MessageBox.Show("Huỷ hẹn giờ thành công!");;
+        }
+    }
+}
